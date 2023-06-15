b6a51918c5f3c03b33628bd3769ad7781054ce44
Rick Koetter <rickkoe@gmail.com>
2023-06-15 14:29:28 -0500

    working, but still can't start with no aliases.

--- san_project/san_app/static/san_app/js/aliases.js
+++ san_project/san_app/static/san_app/js/aliases.js
@@ -10,13 +10,21 @@ var aliasTable;
 
 $(document).ready(function() {
     var container = document.getElementById('aliasTable');
+    console.log(typeof data);  // Outputs: "object" because in JavaScript, arrays are technically a type of object
+    console.log(data.length)
+      // Check if data array is empty and add an empty row if necessary
 
+      if (typeof data === 'undefined' || data.length == 0) {
+        data = [[]];
+    }
+    // console.log(typeof data);  // Outputs: "object" because in JavaScript, arrays are technically a type of object
+    console.log(data.length)
     aliasTable = new Handsontable(container, {
         licenseKey: 'non-commercial-and-evaluation',
         data: data,
         minRows: 5,
         minCols: 3,
-        rowHeaders: true,
+        rowHeaders: false,
         colHeaders: ["ID", "Alias Name", "WWPN", "Use"],
         contextMenu: ['row_above', 'row_below', 'remove_row', '---------', 'undo', 'redo'],  // Custom context menu options
         minSpareRows: 1,  // Always leave one spare row at the end
@@ -25,6 +33,19 @@ $(document).ready(function() {
             if (col === 0) {
                 return {readOnly: true};
             }
+        },
+        beforeChange: function(changes) {
+            changes.forEach(function(change) {
+                if (change[1] === 'WWPN') {  // If the change is in the 'WWPN' column
+                    var newValue = change[3];
+                    if (/^[0-9a-fA-F]{16}$/.test(newValue)) {  // If it's 16 hexadecimal characters without colons
+                        change[3] = newValue.replace(/(.{2})(?=.)/g, '$1:');  // Insert colons
+                    } else if (!/^([0-9a-fA-F]{2}:){7}[0-9a-fA-F]{2}$/.test(newValue)) {  // If it's not 16 hexadecimal characters with colons
+                        change[3] = null;  // Discard the change
+                        alert('Invalid WWPN format!');
+                    }
+                }
+            });
         }
     });
   });
