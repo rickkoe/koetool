81256a88b72b29648bc8ee9d54fd2879c3707bda
Rick Koetter <rickkoe@gmail.com>
2023-06-15 11:29:55 -0500

    index works with deleting and updating aliases!

--- san_project/san_app/static/san_app/js/index.js
+++ san_project/san_app/static/san_app/js/index.js
@@ -1,19 +1,69 @@
-const container = document.getElementById('hot');
-const settings = {
-  licenseKey: 'non-commercial-and-evaluation',  
-  data: [],
-  rowHeaders: true,
-  colHeaders: ['ID', 'Alias Name', 'WWPN', 'Use'],
-  contextMenu: true,
-  columns: [
-    {data: 'id', readOnly: true},  
-    {data: 'alias_name'},
-    {data: 'wwpn'},
-    {data: 'use'}
-  ],
-  afterChange: function (changes, source) {
-    // Handle changes here
-  }
-};
+$.ajaxSetup({
+  headers: { "X-CSRFToken": getCookie("csrftoken") }
+});
 
-const hot = new Handsontable(container, settings);
+var aliasTable;
+
+$(document).ready(function() {
+    var container = document.getElementById('aliasTable');
+    aliasTable = new Handsontable(container, {
+        licenseKey: 'non-commercial-and-evaluation',
+        data: data,
+        minRows: 5,
+        minCols: 3,
+        rowHeaders: true,
+        colHeaders: ["ID", "Alias Name", "WWPN", "Use"],
+        contextMenu: ['row_above', 'row_below', 'remove_row', '---------', 'undo', 'redo'],  // Custom context menu options
+        // minSpareRows: 1,  // Always leave one spare row at the end
+        // Disable ID column
+        cells: function(row, col, prop) {
+            if (col === 0) {
+                return {readOnly: true};
+            }
+        }
+    });
+  });
+  
+   
+  $('#submit-data').click(function() {
+    var data = aliasTable.getData().map(function(row) {
+        if (row[1] || row[2] || row[3]) {  // Only send rows that have at least one of these fields filled
+            return {
+                id: row[0],
+                alias_name: row[1],
+                WWPN: row[2],
+                use: row[3]
+            };
+        }
+    });
+
+    // Filter out any undefined entries (rows that didn't pass the check)
+    data = data.filter(function(entry) { return entry !== undefined; });
+
+    $.ajax({
+        type: 'POST',
+        url: '',
+        data: {
+            'data': JSON.stringify(data),
+            'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
+        },
+        success: function() {
+            location.reload();
+        },
+    });
+});
+
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
--- san_project/san_app/views.py
+++ san_project/san_app/views.py
@@ -7,10 +7,38 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.db import IntegrityError
+from django.http import JsonResponse  # To send JSON response
+from django.views.decorators.csrf import csrf_exempt  # To exempt this view from CSRF protection
+import json  # To parse and generate JSON
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+@csrf_exempt
 def index(request):
-    aliases = SANAlias.objects.all()
-    return render(request, 'index.html', {'aliases': aliases})
+    if request.method == 'POST':
+        data = json.loads(request.POST['data'])
+        
+        logger.debug(f"Received data: {data}")  # Add this line
+
+        # Update existing records and add new ones
+        for row in data:
+            print(row['id'])
+            if row['id']:  # If there's an ID, update the record
+                SANAlias.objects.filter(id=row['id']).update(alias_name=row['alias_name'], WWPN=row['WWPN'], use=row['use'])
+            else:  # If there's no ID, create a new record
+                SANAlias.objects.create(alias_name=row['alias_name'], WWPN=row['WWPN'], use=row['use'])
+
+        # Find and delete records that are not in the submitted data
+        # We only check the IDs of the rows that had an ID (i.e., not the new rows)
+        SANAlias.objects.exclude(id__in=[row['id'] for row in data if row['id']]).delete()
+
+        return JsonResponse({'status': 'success'})
+    else:
+        # For GET requests, we just send all the records to the template
+        aliases = SANAlias.objects.values()
+        return render(request, 'index.html', {'aliases': list(aliases)})
 
 
 def add_alias(request):
