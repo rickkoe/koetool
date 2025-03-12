e1397eca38843aa1e12fdabb73381f8404785b0a
Rick Koetter <rickkoe@gmail.com>
2025-03-12 14:00:20 -0500

    updates?

--- san_project/san_app/views.py
+++ san_project/san_app/views.py
@@ -690,64 +690,68 @@ def create_zone_command_dict():
     all_zones = Zone.objects.select_related('fabric').prefetch_related('members').filter(create='True', fabric__project=config.project).order_by('id')
     firstpass = False # Set trigger for Cisco config t command
     for zone in all_zones:
-        zone_members = zone.members.all()
+        zone_members = zone.members.filter(include_in_zoning=True)
         zone_member_list = []
         for zone_member in zone_members:
             zone_member_list.append(zone_member.name)
+        zone_member_length = len(zone_member_list)
+        print(zone_member_length)
         key = zone.fabric.name
         if key not in zone_command_dict:
             zone_command_dict[key].extend(['',f'### ZONE COMMANDS FOR {key.upper()} '])
         if key not in zoneset_command_dict:
             zoneset_command_dict[key].extend(['',f'### ZONESET COMMANDS FOR {key.upper()} '])
-        if config.san_vendor == 'CI':
-            if firstpass == False:
-                zone_command_dict[key].append('config t')
-                firstpass = True
-            if len(zoneset_command_dict[key]) == 2:
-                zoneset_command_dict[key].append(f'zoneset name {zone.fabric.zoneset_name} vsan {zone.fabric.vsan}')
-            zone_command_dict[key].append(f'zone name {zone.name} vsan {zone.fabric.vsan}')
-            if zone.exists == False:
-                zoneset_command_dict[key].append(f'member {zone.name}')
-            for zone_member in zone_members:
-                if config.cisco_alias == 'fcalias':  
-                    zone_command_dict[key].append(f'member {alias_type} {zone_member.name}')
-                elif config.cisco_alias == 'device-alias' and zone.zone_type == 'smart_peer':
-                    zone_command_dict[key].append(f'member {alias_type} {zone_member.name} {zone_member.use}')
-                elif config.cisco_alias == 'device-alias' and zone.zone_type == 'standard':
-                    zone_command_dict[key].append(f'member {alias_type} {zone_member.name}')
-                elif config.cisco_alias == 'wwpn':
-                    if zone.zone_type == 'smart_peer':
-                        zone_command_dict[key].append(f'member pwwn {zone_member.wwpn} {zone_member.use}')
-                    elif zone.zone_type == 'standard':
-                        zone_command_dict[key].append(f'member pwwn {zone_member.wwpn}')
-        if config.san_vendor == 'BR':
-            if zone.zone_type == 'standard':
-                zone_member_list = ';'.join(zone_member_list)
-                if zone.exists == True:
-                    zone_command_dict[key].append(f'zoneadd "{zone.name}", "{zone_member_list}"')
+        if zone_member_length > 0:
+            if config.san_vendor == 'CI':
+                if firstpass == False:
+                    zone_command_dict[key].append('config t')
+                    firstpass = True
+                if len(zoneset_command_dict[key]) == 2:
+                    zoneset_command_dict[key].append(f'zoneset name {zone.fabric.zoneset_name} vsan {zone.fabric.vsan}')
+                zone_command_dict[key].append(f'zone name {zone.name} vsan {zone.fabric.vsan}')
+                if zone.exists == False:
+                    zoneset_command_dict[key].append(f'member {zone.name}')
+                for zone_member in zone_members:
+                    if config.cisco_alias == 'fcalias':  
+                        zone_command_dict[key].append(f'member {alias_type} {zone_member.name}')
+                    elif config.cisco_alias == 'device-alias' and zone.zone_type == 'smart_peer':
+                        zone_command_dict[key].append(f'member {alias_type} {zone_member.name} {zone_member.use}')
+                    elif config.cisco_alias == 'device-alias' and zone.zone_type == 'standard':
+                        zone_command_dict[key].append(f'member {alias_type} {zone_member.name}')
+                    elif config.cisco_alias == 'wwpn':
+                        if zone.zone_type == 'smart_peer':
+                            zone_command_dict[key].append(f'member pwwn {zone_member.wwpn} {zone_member.use}')
+                        elif zone.zone_type == 'standard':
+                            zone_command_dict[key].append(f'member pwwn {zone_member.wwpn}')
+            if config.san_vendor == 'BR':
+                if zone.zone_type == 'standard':
+                    zone_member_list = ';'.join(zone_member_list)
+                    if zone.exists == True:
+                        zone_command_dict[key].append(f'zoneadd "{zone.name}", "{zone_member_list}"')
+                    elif zone.exists == False:
+                        zone_command_dict[key].append(f'zonecreate "{zone.name}", "{zone_member_list}"')
+                elif zone.zone_type == 'smart_peer':
+                    initiators = ';'.join([alias.name for alias in zone_members if alias.use == 'init'])
+                    targets = ';'.join([alias.name for alias in zone_members if alias.use == 'target'])
+                    if zone.exists == True:
+                        if targets: 
+                            principal = f' -principal "{targets}"'
+                        else:
+                            principal = ''
+                        if initiators:
+                            members = f' -members "{initiators}"'
+                        else:
+                            members = ''
+                        zone_command_dict[key].append(f'zoneadd --peerzone "{zone.name}"{principal}{members}')
+                    elif zone.exists == False:
+                        zone_command_dict[key].append(f'zonecreate --peerzone "{zone.name}" -principal "{targets}" -members "{initiators}"')
+                if len(zoneset_command_dict[key]) == 2 and zone.fabric.exists == False and zone.exists == False:
+                    zoneset_command_dict[key].append(f'cfgcreate "{zone.fabric.zoneset_name}", "{zone.name}"')
                 elif zone.exists == False:
-                    zone_command_dict[key].append(f'zonecreate "{zone.name}", "{zone_member_list}"')
-            elif zone.zone_type == 'smart_peer':
-                initiators = ';'.join([alias.name for alias in zone_members if alias.use == 'init'])
-                targets = ';'.join([alias.name for alias in zone_members if alias.use == 'target'])
-                if zone.exists == True:
-                    if targets: 
-                        principal = f' -principal "{targets}"'
-                    else:
-                        principal = ''
-                    if initiators:
-                        members = f' -members "{initiators}"'
-                    else:
-                        members = ''
-                    zone_command_dict[key].append(f'zoneadd --peerzone "{zone.name}"{principal}{members}')
-                elif zone.exists == False:
-                    zone_command_dict[key].append(f'zonecreate --peerzone "{zone.name}" -principal "{targets}" -members "{initiators}"')
-            if len(zoneset_command_dict[key]) == 2 and zone.fabric.exists == False and zone.exists == False:
-                zoneset_command_dict[key].append(f'cfgcreate "{zone.fabric.zoneset_name}", "{zone.name}"')
-            elif zone.exists == False:
-                zoneset_command_dict[key].append(f'cfgadd "{zone.fabric.zoneset_name}", "{zone.name}"')
-            else:
-                print(f'Zone {zone.name} not added to a config:   Fabric: {zone.fabric.exists} Zone: {zone.exists}')
+                    zoneset_command_dict[key].append(f'cfgadd "{zone.fabric.zoneset_name}", "{zone.name}"')
+                else:
+                    pass
+                    # print(f'Zone {zone.name} not added to a config:   Fabric: {zone.fabric.exists} Zone: {zone.exists}')
 
     
     for key in zoneset_command_dict:
